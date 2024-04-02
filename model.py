d020ba4373f5c8951bfdf10e9f9c26be6f9f5d02
Cody Falkosky <codyfalkosky@gmail.com>
2024-04-02 14:48:23 -0700

    added architecture SIMPLER

--- model.py
+++ model.py
@@ -156,6 +156,60 @@ class ObjEncoder:
     
             return model
 
+        #### SIMPLE ####
+        ################
+        
+        elif architecture == 'SIMPLER':
+            '''
+            Architecture for an object encoder taking an input of [image_tensor (32, 40, 3), coords_tensor (4,) cxcywh]
+            and returning a vector of depth 256 encoding the image
+            '''
+    
+            clips_in = Input([32, 40, 3], name='clips_input')
+    
+            ######## CLIP STAGE ########
+            x = clips_in
+            
+            # Block 1   
+            x = Conv2D(8, (3, 3), padding='same', name='clip_block1_1')(x)
+            x = ReLU(                              name='clip_block1_3')(x)    
+            x = MaxPool2D((2, 2), 2,               name='clip_block1_7')(x)
+    
+            # Block 2
+            x = Conv2D(16, (3, 3), padding='same', name='clip_block2_1')(x)
+            x = ReLU(                              name='clip_block2_3')(x)    
+            x = MaxPool2D((2, 2), 2,               name='clip_block2_7')(x)
+            
+            clips_out = Flatten()(x)
+    
+            ######## COORDS STAGE ########
+    
+            coords_in = Input([4], name='coords_input')
+    
+            x = coords_in
+            # Block 1
+            x = Dense(8,          name='coords_block1_1')(x)
+            x = BatchNormalization(name='coords_block1_2')(x)
+            x = ReLU(              name='coords_block1_3')(x)
+    
+            coords_out = x
+    
+            ######## COMBINED STAGE ########
+            
+            x = Concatenate(       name='concatenate')([coords_out, clips_out])
+    
+            # Block 1
+            x = Dense(16,          name='combined_block1_1')(x)
+            x = BatchNormalization(name='combined_block1_2')(x)
+            x = ReLU(              name='combined_block1_3')(x)
+
+    
+            x = Dense(8,         name='output')(x)        
+    
+            model = tf.keras.Model(inputs=[clips_in, coords_in], outputs=[x], name='obj_encoder')
+    
+            return model
+
 
 def _build_model(architecture):
     if architecture == 'MODEL1':
